2e536a44c61b0e15e6572993ef97c549dbfd776f
A00452906 <90960064+A00452906@users.noreply.github.com>
2022-03-01 23:30:12 -0400

    Update urls.py
    removed unnecessary code

--- Django_Assignment_App/urls.py
+++ Django_Assignment_App/urls.py
@@ -2,8 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns= [
-   # path("Get_Hotel_list/", views.Get_Hotel_List, name="hotelList"),
-   # path("hotel_list/<str:pk>", views.Hotels_detail, name="hotelDetail"),
-  #  path("generics_hotel_list/", views.get_generics_list.as_view(), name="genericsList"),
    path("hotel_list/", views.hotel_list)
 ]
